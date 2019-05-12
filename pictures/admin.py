69f43e03bec8766349fa0a61b98558520f7a0c5b
OkothPius <orukopius8@gmail.com>
2019-05-12 11:21:27 +0300

    add location filter

--- pictures/admin.py
+++ pictures/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from .models import Editor,Location,Category,Image
 
+class ImageAdmin(admin.ModelAdmin):
+    filter_horizontal =('Location',)
+
 # Register your models here.
 admin.site.register(Editor)
 admin.site.register(Location)
