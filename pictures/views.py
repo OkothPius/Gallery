7a14f8a76ee83fc83c8c575352ed3203dad1068e
OkothPius <orukopius8@gmail.com>
2019-05-13 00:41:23 +0300

    import Image

--- pictures/views.py
+++ pictures/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render,redirect
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
 import datetime as dt 
+from django.http import HttpResponse, Http404
 from .models import Image
 
 # Create your views here.
@@ -11,22 +11,21 @@ def welcome(request):
 def pictures(request):
     image = Image.objects.all()
     return render(request, 'pictures.html', {"image":image})
+
+def image(request,image_id):
+    image = Image.objects.get(id = image_id) 
+    return render(request, "image.html", {"image":image})        
+
     
 def search_results(request):
-    if 'pictures' in request.GET and request.GET["pictures"]:
-        search_term = request.GET.get("pictures")
-        searched_pictures = Pictures.search_by_image_category(search_term)
+    if 'images' in request.GET and request.GET["images"]:
+        search_term = request.GET.get("images")
+        searched_images = images.search_by_image_category(search_term)
         message = f"{search_term}"
 
-        return render(request, 'search.html',{"message":message,"pictures": searched_pictures})
+        return render(request, 'search.html',{"message":message,"images": searched_images})
 
     else:
         message = "You haven't searched for any term yet"
         return render(request, 'search.html',{"message":message})
 
-def image(request,image_id):
-    try:
-        image = Image.objects.get(id = image_id)
-    except DoesNotExist:
-        raise Http404()    
-    return render(request, "image.html", {"image":image})        
