207e6c43471c9832811fc20eab0ac8ff55f173a4
OkothPius <orukopius8@gmail.com>
2019-05-12 11:23:08 +0300

    remove pictures form the regex

--- pictures/urls.py
+++ pictures/urls.py
@@ -1,11 +1,10 @@
 from django.conf import settings
 from django.conf.urls.static import static
-from django.http import HttpResponse,Http404
 from django.conf.urls import url
 from . import views
 
 urlpatterns=[
-    url(r'^pictures/', views.pictures, name='pictures'),
+    url(r'^$', views.pictures, name='pictures'),
     url(r'^search/', views.search_results, name='search_results'), 
     url(r'^image/(\d+)',views.image,name='image') 
 ]
