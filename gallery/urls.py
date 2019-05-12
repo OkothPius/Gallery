792ef3a2559c9659f0850632120f757710ba1000
OkothPius <orukopius8@gmail.com>
2019-05-13 00:47:48 +0300

    add all

--- gallery/urls.py
+++ gallery/urls.py
@@ -17,7 +17,6 @@ from django.conf.urls import url,include
 from django.contrib import admin
 
 urlpatterns = [
-    # path('', include('pictures.urls')),
     url(r'^admin/', admin.site.urls),
-    url(r'',include('pictures.urls'))
+    url(r'', include('pictures.urls'))
 ]
