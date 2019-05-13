5f68d10c6e7cb1184282810f73e2085bc8dc596f
OkothPius <orukopius8@gmail.com>
2019-05-13 16:11:20 +0300

    add images

--- pictures/models.py
+++ pictures/models.py
@@ -77,7 +77,7 @@ class Image(models.Model):
 
     @classmethod
     def search_by_image_category(cls,search_term):
-        pictures = cls.objects.filter(image_category__icontains=search_term)
+        pictures = cls.objects.filter(image_name__icontains=search_term)
         return pictures
     
 
--- pictures/views.py
+++ pictures/views.py
@@ -4,26 +4,22 @@ from django.http import HttpResponse, Http404
 from .models import Image
 
 # Create your views here.
-def welcome(request):
-    welcome = Welcome.objects.all()
-    return render(request, 'welcome.html', {"welcome":welcome})
-
 def pictures(request):
     image = Image.objects.all()
     return render(request, 'pictures.html', {"image":image})
 
-def image(request,image_id):
+def single_image(request,image_id):
     image = Image.objects.get(id = image_id) 
     return render(request, "image.html", {"image":image})        
 
     
 def search_results(request):
-    if 'images' in request.GET and request.GET["images"]:
-        search_term = request.GET.get("images")
-        searched_images = images.search_by_image_category(search_term)
+    if 'image' in request.GET and request.GET["image"]:
+        search_term = request.GET.get("image")
+        searched_images = Image.search_by_image_category(search_term)
         message = f"{search_term}"
 
-        return render(request, 'search.html',{"message":message,"images": searched_images})
+        return render(request, 'search.html',{"message":message,"image": searched_images})
 
     else:
         message = "You haven't searched for any term yet"
